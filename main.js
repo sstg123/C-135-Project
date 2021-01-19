5fb486e16e6ea4958a00734fa703bd8f4bebf712
sstg123 <71703766+sstg123@users.noreply.github.com>
2021-01-19 20:01:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,9 +30,35 @@ function gotResult(error, results) {
   objects = results;
 }
 
-function draw()
-
-{
+function draw() {
   image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+          
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 
+         
+          if(objects[i].label == object_name)
+          {
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("object_status").innerHTML = object_name + " Found";
+            synth = window.speechSynthesis;
+            utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+            synth.speak(utterThis);
+          }
+          else
+          {
+            document.getElementById("object_status").innerHTML = object_name + " Not Found";
+          }          
+         }
+      }
 }
